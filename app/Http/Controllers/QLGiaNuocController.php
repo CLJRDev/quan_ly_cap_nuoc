59b410a3aee7551d166a0c7a3e90a80ab80d5c37
CLJR <nguyenconglam2002a@gmail.com>
2024-05-14 00:00:04 +0700

    fe - gia nuoc

--- app/Http/Controllers/QLGiaNuocController.php
+++ app/Http/Controllers/QLGiaNuocController.php
@@ -6,218 +6,213 @@ use App\Http\Controllers\Controller;
 use App\Models\QLGiaNuocModel;
 use Illuminate\Http\Request;
 use \Illuminate\Support\Facades\Validator;
-use Illuminate\Database\Eloquent\ModelNotFoundException; 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\Rule;
 
 class QLGiaNuocController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return QLGiaNuocModel::select('*','dm_loaikhachhang.ten_loai_khach_hang')
-        ->join('dm_loaikhachhang','dm_loaikhachhang.ma_loai_khach_hang','=','ql_nhomgia.ma_loai_khach_hang')
-        ->orderBy('ma_nhom_gia', 'ASC')->get();
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return QLGiaNuocModel::select('*', 'dm_loaikhachhang.ten_loai_khach_hang')
+      ->join('dm_loaikhachhang', 'dm_loaikhachhang.ma_loai_khach_hang', '=', 'ql_nhomgia.ma_loai_khach_hang')
+      ->orderBy('ma_nhom_gia', 'ASC')->get();
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $message = [
-            'required' => 'Xin hãy điền đủ thông tin!',
-            'required_if' => 'Xin hãy điền đủ thông tin!',
-            'unique' => 'Cỡ đồng hồ đã tồn tại!',
-        ];
-        $validator = Validator::make($request->all(),[
-            'ten_nhom_gia' => 'required|unique:ql_nhomgia,ten_nhom_gia',
-            'hs_duoi_10m' => 'required_if:hs_rieng,NULL',
-            'hs_tu_10m_den_20m' => 'required_if:hs_rieng,NULL',
-            'hs_tu_20m_den_30m' => 'required_if:hs_rieng,NULL',
-            'hs_tren_30m' => 'required_if:hs_rieng,NULL',
-            'hs_rieng' => 'required_if:hs_duoi_10m,NULL',
-            'gia_ban' => 'required',
-            'ma_loai_khach_hang' => 'required|unique:ql_nhomgia,ma_loai_khach_hang',
-          ],$message);
-        
-        if($validator->fails()){
-            return response()->json([
-                'error' => $validator->errors(),
-                ],422);
-        }
-        $nhom_gia = new QLGiaNuocModel; 
-        $nhom_gia->ten_nhom_gia=$request->ten_nhom_gia;
-        if($request->has('hs_duoi_10m')&&$request->has('hs_tu_10m_den_20m')&&$request->has('hs_tu_20m_den_30m')&&$request->has('hs_tren_30m')){
-            $nhom_gia->hs_duoi_10m=$request->hs_duoi_10m;
-            $nhom_gia->hs_tu_10m_den_20m=$request->hs_tu_10m_den_20m;
-            $nhom_gia->hs_tu_20m_den_30m=$request->hs_tu_20m_den_30m;
-            $nhom_gia->hs_tren_30m=$request->hs_tren_30m;
-        }
-        else{
-            $nhom_gia->hs_rieng=$request->hs_rieng;
-        }
-        $nhom_gia->gia_ban=$request->gia_ban;
-        $nhom_gia->ma_loai_khach_hang=$request->ma_loai_khach_hang;
-        $result = $nhom_gia->save();
-        if($result){
-            return response()->json([
-                'message' => 'Tạo thành công!'
-              ]);
-        }
-        else{
-            return response()->json([
-                'error' => 'Lỗi!'
-              ],422);
-        }
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $message = [
+      'required' => 'Xin hãy điền đủ thông tin!',
+      'required_if' => 'Xin hãy điền đủ thông tin!',
+      'unique' => 'Nhóm giá đã tồn tại!',
+    ];
+    $validator = Validator::make($request->all(), [
+      'ten_nhom_gia' => 'required|unique:ql_nhomgia,ten_nhom_gia',
+      'hs_duoi_10m' => 'required_if:hs_rieng,NULL',
+      'hs_tu_10m_den_20m' => 'required_if:hs_rieng,NULL',
+      'hs_tu_20m_den_30m' => 'required_if:hs_rieng,NULL',
+      'hs_tren_30m' => 'required_if:hs_rieng,NULL',
+      'hs_rieng' => 'required_if:hs_duoi_10m,NULL',
+      'gia_ban' => 'required',
+      'ma_loai_khach_hang' => 'required|unique:ql_nhomgia,ma_loai_khach_hang',
+    ], $message);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        try{
-            return QLGiaNuocModel::where("ma_nhom_gia",$id)->firstOrFail();
-        }catch (ModelNotFoundException $e) {
-            return response()->json([
-               'error' => 'Nhóm giá không tồn tại!'
-            ],422);
-        }
+    if ($validator->fails()) {
+      return response()->json([
+        'error' => $validator->errors(),
+      ], 422);
     }
+    $nhom_gia = new QLGiaNuocModel;
+    $nhom_gia->ten_nhom_gia = $request->ten_nhom_gia;
+    if ($request->has('hs_duoi_10m') && $request->has('hs_tu_10m_den_20m') && $request->has('hs_tu_20m_den_30m') && $request->has('hs_tren_30m')) {
+      $nhom_gia->hs_duoi_10m = $request->hs_duoi_10m;
+      $nhom_gia->hs_tu_10m_den_20m = $request->hs_tu_10m_den_20m;
+      $nhom_gia->hs_tu_20m_den_30m = $request->hs_tu_20m_den_30m;
+      $nhom_gia->hs_tren_30m = $request->hs_tren_30m;
+    } else {
+      $nhom_gia->hs_rieng = $request->hs_rieng;
+    }
+    $nhom_gia->gia_ban = $request->gia_ban;
+    $nhom_gia->ma_loai_khach_hang = $request->ma_loai_khach_hang;
+    $result = $nhom_gia->save();
+    if ($result) {
+      return response()->json([
+        'message' => 'Tạo thành công!'
+      ]);
+    } else {
+      return response()->json([
+        'error' => 'Lỗi!'
+      ], 422);
+    }
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+    try {
+      return QLGiaNuocModel::select('*', 'dm_loaikhachhang.ten_loai_khach_hang')
+        ->join('dm_loaikhachhang', 'dm_loaikhachhang.ma_loai_khach_hang', '=', 'ql_nhomgia.ma_loai_khach_hang')
+        ->where("ma_nhom_gia", $id)->firstOrFail();
+    } catch (ModelNotFoundException $e) {
+      return response()->json([
+        'error' => 'Nhóm giá không tồn tại!'
+      ], 422);
     }
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $message = [
-            'required' => 'Xin hãy điền đủ thông tin!',
-            'required_if' => 'Xin hãy điền đủ thông tin!',
-            'unique' => 'Cỡ đồng hồ đã tồn tại!',
-        ];
-        $validator = Validator::make($request->all(),[
-            'ten_nhom_gia' => [
-                'required',
-                Rule::unique('ql_nhomgia', 'ten_nhom_gia')->ignore($id, 'ma_nhom_gia')
-              ],
-            'hs_duoi_10m' => 'required_if:hs_rieng,NULL',
-            'hs_tu_10m_den_20m' => 'required_if:hs_rieng,NULL',
-            'hs_tu_20m_den_30m' => 'required_if:hs_rieng,NULL',
-            'hs_tren_30m' => 'required_if:hs_rieng,NULL',
-            'hs_rieng' => 'required_if:hs_duoi_10m,NULL',
-            'gia_ban' => 'required',
-            'ma_loai_khach_hang' => [
-                'required',
-                Rule::unique('ql_nhomgia', 'ma_loai_khach_hang')->ignore($id, 'ma_nhom_gia')
-              ],
-          ],$message);
-        
-        if($validator->fails()){
-            return response()->json([
-                'error' => $validator->errors(),
-                ],422);
-        }
-        try{
-            $nhom_gia = QLGiaNuocModel::findOrFail($id); 
-            if(isset($request->ten_nhom_gia)){
-                $nhom_gia->ten_nhom_gia=$request->ten_nhom_gia;
-            }
-            $nhom_gia->ten_nhom_gia=$request->ten_nhom_gia;
-            if(isset($request->hs_duoi_10m)){
-                $nhom_gia->hs_duoi_10m=$request->hs_duoi_10m;
-            }
-            if(isset($request->hs_tu_10m_den_20m)){
-                $nhom_gia->hs_tu_10m_den_20m=$request->hs_tu_10m_den_20m;
-            }
-            if(isset($request->hs_tu_20m_den_30m)){
-                $nhom_gia->hs_tu_20m_den_30m=$request->hs_tu_20m_den_30m;
-            }
-            if(isset($request->hs_tren_30m)){
-                $nhom_gia->hs_tren_30m=$request->hs_tren_30m;
-            }
-            if(isset($request->hs_rieng)){
-                $nhom_gia->hs_rieng=$request->hs_rieng;
-            }
-            if(isset($request->gia_ban)){
-                $nhom_gia->gia_ban=$request->gia_ban;
-            }
-            if(isset($request->ma_loai_khach_hang)){
-                $nhom_gia->ma_loai_khach_hang=$request->ma_loai_khach_hang;
-            }
-            $result = $nhom_gia->save();
-        }catch (ModelNotFoundException $e) {
-            return response()->json([
-               'error' => 'Nhóm giá không tồn tại!'
-            ],422);
-        }
-        if($result){
-            return response()->json([
-                'message' => 'Cập nhật thành công!'
-              ]);
-        }
-        else{
-            return response()->json([
-                'error' => 'Lỗi!'
-              ],422);
-        }
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        try{
-            $nhom_gia = QLGiaNuocModel::findOrFail($id);
-            $result = $nhom_gia->delete();
-        }catch (ModelNotFoundException $e) {
-            return response()->json([
-               'error' => 'Nhóm giá không tồn tại!'
-            ],422);
-        }
-        if($result){
-            return response()->json([
-                'message' => 'Xóa thành công!'
-              ]);
-        }
-        else{
-            return response()->json([
-                'error' => 'Lỗi!'
-              ],422);
-        }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    $message = [
+      'required' => 'Xin hãy điền đủ thông tin!',
+      'required_if' => 'Xin hãy điền đủ thông tin!',
+      'unique' => 'Nhóm giá đã tồn tại!',
+    ];
+    $validator = Validator::make($request->all(), [
+      'ten_nhom_gia' => [
+        'required',
+        Rule::unique('ql_nhomgia', 'ten_nhom_gia')->ignore($id, 'ma_nhom_gia')
+      ],
+      'hs_duoi_10m' => 'required_if:hs_rieng,NULL',
+      'hs_tu_10m_den_20m' => 'required_if:hs_rieng,NULL',
+      'hs_tu_20m_den_30m' => 'required_if:hs_rieng,NULL',
+      'hs_tren_30m' => 'required_if:hs_rieng,NULL',
+      'hs_rieng' => 'required_if:hs_duoi_10m,NULL',
+      'gia_ban' => 'required',
+      'ma_loai_khach_hang' => [
+        'required',
+        Rule::unique('ql_nhomgia', 'ma_loai_khach_hang')->ignore($id, 'ma_nhom_gia')
+      ],
+    ], $message);
+
+    if ($validator->fails()) {
+      return response()->json([
+        'error' => $validator->errors(),
+      ], 422);
     }
-    public function search(Request $request)
-    {
-        $query =  QLGiaNuocModel::query()->select('*','dm_loaikhachhang.ten_loai_khach_hang')
-        ->join('dm_loaikhachhang','dm_loaikhachhang.ma_loai_khach_hang','=','ql_nhomgia.ma_loai_khach_hang');
-        if($request->has('ten_nhom_gia')){
-            $query->where("ten_nhom_gia","like","%".$request->ten_nhom_gia."%");
-        }
-        if($request->has('ma_nhom_gia')){
-            $query->where("ma_nhom_gia",$request->ma_nhom_gia);
-        }
-        if($request->has('ma_loai_khach_hang')){
-            $query->where("ma_loai_khach_hang",$request->ma_loai_khach_hang);
-        }
-        $result = $query->orderBy('ma_nhom_gia', 'ASC')->get();
-        return $result;
+    try {
+      $nhom_gia = QLGiaNuocModel::findOrFail($id);
+      if (isset($request->ten_nhom_gia)) {
+        $nhom_gia->ten_nhom_gia = $request->ten_nhom_gia;
+      }
+      $nhom_gia->ten_nhom_gia = $request->ten_nhom_gia;
+      if (isset($request->hs_duoi_10m)) {
+        $nhom_gia->hs_duoi_10m = $request->hs_duoi_10m;
+      }
+      if (isset($request->hs_tu_10m_den_20m)) {
+        $nhom_gia->hs_tu_10m_den_20m = $request->hs_tu_10m_den_20m;
+      }
+      if (isset($request->hs_tu_20m_den_30m)) {
+        $nhom_gia->hs_tu_20m_den_30m = $request->hs_tu_20m_den_30m;
+      }
+      if (isset($request->hs_tren_30m)) {
+        $nhom_gia->hs_tren_30m = $request->hs_tren_30m;
+      }
+      if (isset($request->hs_rieng)) {
+        $nhom_gia->hs_rieng = $request->hs_rieng;
+      }
+      if (isset($request->gia_ban)) {
+        $nhom_gia->gia_ban = $request->gia_ban;
+      }
+      if (isset($request->ma_loai_khach_hang)) {
+        $nhom_gia->ma_loai_khach_hang = $request->ma_loai_khach_hang;
+      }
+      $result = $nhom_gia->save();
+    } catch (ModelNotFoundException $e) {
+      return response()->json([
+        'error' => 'Nhóm giá không tồn tại!'
+      ], 422);
     }
+    if ($result) {
+      return response()->json([
+        'message' => 'Cập nhật thành công!'
+      ]);
+    } else {
+      return response()->json([
+        'error' => 'Lỗi!'
+      ], 422);
+    }
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    try {
+      $nhom_gia = QLGiaNuocModel::findOrFail($id);
+      $result = $nhom_gia->delete();
+    } catch (ModelNotFoundException $e) {
+      return response()->json([
+        'error' => 'Nhóm giá không tồn tại!'
+      ], 422);
+    }
+    if ($result) {
+      return response()->json([
+        'message' => 'Xóa thành công!'
+      ]);
+    } else {
+      return response()->json([
+        'error' => 'Lỗi!'
+      ], 422);
+    }
+  }
+  public function search(Request $request)
+  {
+    $query =  QLGiaNuocModel::query()->select('*', 'dm_loaikhachhang.ten_loai_khach_hang')
+      ->join('dm_loaikhachhang', 'dm_loaikhachhang.ma_loai_khach_hang', '=', 'ql_nhomgia.ma_loai_khach_hang');
+    if ($request->has('ten_nhom_gia')) {
+      $query->where("ten_nhom_gia", "like", "%" . $request->ten_nhom_gia . "%");
+    }
+    if ($request->has('ma_loai_khach_hang')) {
+      $query->where("dm_loaikhachhang.ma_loai_khach_hang", $request->ma_loai_khach_hang);
+    }
+    $result = $query->orderBy('ma_nhom_gia', 'ASC')->get();
+    return $result;
+  }
 }
---
+++ react/src/components/khach_hang_thanh_toan/QL_gia_nuoc/SuaGia.jsx
@@ -0,0 +1,138 @@
+import { Link, useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from "react"
+import Select from 'react-select'
+import { MdOutlineEdit } from "react-icons/md";
+
+export default function SuaGia() {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [loaiKhachHangs, setLoaiKhachHangs] = useState(null)
+  const [gia, setGia] = useState({
+    ten_nhom_gia: '',
+    hs_duoi_10m: '',
+    hs_tu_10m_den_20m: '',
+    hs_tu_20m_den_30m: '',
+    hs_tren_30m: '',
+    hs_rieng: '',
+    gia_ban: ''
+  })
+  const [selectedOption, setSelectedOption] = useState({})
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/api/loai_khach_hang`)
+      .then(response => {
+        setLoaiKhachHangs(response.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/api/nhom_gia/${id}`)
+      .then(response => {
+        setGia(response.data)
+        setSelectedOption({value: response.data.ma_loai_khach_hang, label: response.data.ten_loai_khach_hang})
+      })
+  }, [])
+
+  if (!loaiKhachHangs) return null
+  const options = []
+  loaiKhachHangs.forEach(item => {
+    options.push({
+      value: item.ma_loai_khach_hang,
+      label: item.ten_loai_khach_hang
+    })
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setGia(pre => {
+      return {
+        ...pre,
+        [name]: value
+      }
+    })
+  }
+
+  const handleSelectChange = (option) => {
+    setSelectedOption(option)
+  }
+
+  const sua = async () => {
+    const formData = new FormData()
+    formData.append('_method', 'PUT')
+    formData.append('ten_nhom_gia', gia.ten_nhom_gia)
+    if(gia.hs_rieng === null) {
+      formData.append('hs_duoi_10m', gia.hs_duoi_10m)
+      formData.append('hs_tu_10m_den_20m', gia.hs_tu_10m_den_20m)
+      formData.append('hs_tu_20m_den_30m', gia.hs_tu_20m_den_30m)
+      formData.append('hs_tren_30m', gia.hs_tren_30m)
+    }else{
+      formData.append('hs_rieng', gia.hs_rieng)
+    }
+    formData.append('gia_ban', gia.gia_ban)
+    formData.append('ma_loai_khach_hang', selectedOption.value)
+
+    try {
+      const response = await axios.post(`http://127.0.0.1:8000/api/nhom_gia/${id}`, formData)
+      console.log(response.data.message)
+      navigate('/gia_nuoc')
+    } catch (error) {
+      console.log(error.response.data.error)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await sua()
+  }
+
+  return (
+    <div className="page">
+      <h2 className="title">Sửa giá nước</h2>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="ten_nhom_gia">Tên nhóm giá</label>
+          <input type="text" id='ten_nhom_gia' name='ten_nhom_gia' onChange={handleInputChange} value={gia.ten_nhom_gia} />
+        </div>
+        <div>
+          <label htmlFor="loai_khach_hang">Loại khách hàng</label>
+          <Select
+            options={options}
+            onChange={handleSelectChange}
+            value={selectedOption}
+          />
+        </div>
+        <div>
+          <label htmlFor="hs_duoi_10m">Hệ số dưới 10m³</label>
+          <input type="number" id='hs_duoi_10m' step='0.01' name='hs_duoi_10m' onChange={handleInputChange} value={!gia.hs_duoi_10m ? '' : gia.hs_duoi_10m} />
+        </div>
+        <div>
+          <label htmlFor="hs_tu_10m_den_20m">Hệ số từ 10m³ đến 20m³</label>
+          <input type="number" id='hs_tu_10m_den_20m' step='0.01' name='hs_tu_10m_den_20m' onChange={handleInputChange} value={!gia.hs_tu_10m_den_20m ? '' : gia.hs_tu_10m_den_20m} />
+        </div>
+        <div>
+          <label htmlFor="hs_tu_20m_den_30m">Hệ số từ 20m³ đến 30m³</label>
+          <input type="number" id='hs_tu_20m_den_30m' step='0.01' name='hs_tu_20m_den_30m' onChange={handleInputChange} value={!gia.hs_tu_20m_den_30m ? '' : gia.hs_tu_20m_den_30m} />
+        </div>
+        <div>
+          <label htmlFor="hs_tren_30m">Hệ số trên 30m³</label>
+          <input type="number" id='hs_tren_30m' step='0.01' name='hs_tren_30m' onChange={handleInputChange} value={!gia.hs_tren_30m ? '' : gia.hs_tren_30m} />
+        </div>
+        <div>
+          <label htmlFor="hs_rieng">Hệ số riêng</label>
+          <input type="number" id='hs_rieng' step='0.01' name='hs_rieng' onChange={handleInputChange} value={!gia.hs_rieng ? '' : gia.hs_rieng} />
+        </div>
+        <div>
+          <label htmlFor="gia_ban">Giá bán</label>
+          <input type="number" id='gia_ban' step='0.01' name='gia_ban' onChange={handleInputChange} value={gia.gia_ban} />
+        </div>
+        <div>
+          <button className="btn-add">
+            <MdOutlineEdit style={{ transform: 'scale(1.2)' }} />
+            &nbsp; Sửa nhóm giá
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
