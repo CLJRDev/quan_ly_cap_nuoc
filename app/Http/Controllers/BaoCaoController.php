88a8d0fcf4970d13ed0746d35ef8bd23e50324f8
Đỗ Mạnh <mashiro1420@gmail.com>
2024-06-04 22:22:47 +0700

    be-dashboard

--- app/Http/Controllers/BaoCaoController.php
+++ app/Http/Controllers/BaoCaoController.php
@@ -98,8 +98,7 @@ class BaoCaoController extends Controller
         return $ds_khach_co_hd->get();
     }
     public function dashboard(Request $request){
-        $date = strtotime(date("YYYY-mm-dd"));
-        $date = DateTime::createFromFormat("YYYY-mm-dd", date("YYYY-mm-dd"))->format("YYYY-mm-dd");
+        $date = \DateTime::createFromFormat("Y-m-d", date("Y-m-d"));
         $tong_khach_hang=QLKhachHangModel::selectRaw('count(ql_khachhang.ma_khach_hang) as so_khach_hang')->get();
         $tong_hop_dong=QLHopDongModel::selectRaw('count(ql_hopdong.ma_hop_dong) as so_hop_dong')->get();
         $tong_dh_khoi=QLDongHoKhoiModel::selectRaw('count(ql_donghokhoi.ma_dong_ho) as so_dh_khoi')->get();
@@ -115,7 +114,7 @@ class BaoCaoController extends Controller
         $tong_khach_hang_moi_khong_lap_dat = QLKhachHangModel::selectRaw('count(ql_khachhang.ma_khach_hang) as so_khach_hang_moi_khong_lap_dat')
             ->leftJoin('ql_hopdong','ql_hopdong.ma_khach_hang','=','ql_khachhang.ma_khach_hang')
             ->leftJoin('ql_lapdatdhkhach','ql_hopdong.ma_hop_dong','=','ql_lapdatdhkhach.ma_hop_dong')
-            ->whereRaw('ql_hopdong.ma_lap_dat is null')
+            ->whereRaw('ql_hopdong.ma_hop_dong is null')
             ->whereBetween('ngay_dang_ky',[date_sub($date,date_interval_create_from_date_string("1 month")),$date])
             ->get();
         return response()->json([
@@ -135,7 +134,8 @@ class BaoCaoController extends Controller
         ->leftJoin('ql_hoadon','ql_hoadon.ma_lap_dat','=','ql_lapdatdhkhach.ma_lap_dat')
         ->where('ma_tuyen',$request->ma_tuyen)
         ->whereRaw('(ql_hoadon.tu_ngay>='.$request->tu_ngay.' and ql_hoadon.den_ngay<='.$request->den_ngay.')')
-        ->whereBetween('ngay_dang_ky',[$request->tu_ngay,$request->den_ngay]);
+        ->whereBetween('ngay_dang_ky',[$request->tu_ngay,$request->den_ngay])
+        ->groupBy('ql_khachhang.ma_khach_hang');
         return $ds_khach_khu_vuc->get();
     }
 }
