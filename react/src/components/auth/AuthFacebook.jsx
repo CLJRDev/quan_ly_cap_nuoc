7a2048889bbc52ccfdc0c972c3616f31f296750a
CLJR <nguyenconglam2002a@gmail.com>
2024-06-05 15:23:51 +0700

    fe - fix fb login

--- react/src/components/auth/AuthFacebook.jsx
+++ react/src/components/auth/AuthFacebook.jsx
@@ -5,19 +5,19 @@ import { useNavigate } from 'react-router-dom'
 export default function AuthFacebook() {
   const navigate = useNavigate()
   console.log(window.location.search)
-  // useEffect(() => {
-  //   axios.get(`http://127.0.0.1:8000/api/auth/facebook/callback${window.location.search}`)
-  //     .then(response => {
-  //       console.log(response)
-  //       const quyens = JSON.stringify(response.data.quyen)
-  //       localStorage.setItem('quyens', quyens)
-  //       localStorage.setItem('user', response.data.nguoi_dung)
-  //       navigate('/dashboard')
-  //     })
-  //     .catch(error => {
-  //       console.log(error)
-  //     })
-  // }, [])
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/api/auth/facebook/callback${window.location.search}`)
+      .then(response => {
+        console.log(response)
+        const quyens = JSON.stringify(response.data.quyen)
+        localStorage.setItem('quyens', quyens)
+        localStorage.setItem('user', response.data.nguoi_dung)
+        navigate('/dashboard')
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
 
   return (
     <div>
